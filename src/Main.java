2f3eafe8f521c444e86990c71a3c7e6c52f1dfe0
NARUTO9556 <gruver5263@gmail.com>
2023-04-26 02:44:13 +0800

    1-3 задача

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //task1();//накопления
-        //task2();//счет
-        //task3();//численность населения
+        task1();//накопления
+        task2();//счет
+        task3();//численность населения
     }
     private static void task1(){
         System.out.println("Задача 1");
